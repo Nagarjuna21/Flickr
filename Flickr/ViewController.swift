0937e7dd6c311357deb7394d4c8fff5a47989347
Nagarjuna Madamanchi <Nagarjuna.Madamanchi@LloydsBanking.com>
2018-10-26 10:39:19 +0100

    Initial Commit

---
+++ Flickr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Flickr
+//
+//  Created by Nagarjuna on 26/10/2018.
+//  Copyright © 2018 Universe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
